2923da9abdf99289f48c66cddb66733e222f3e86
Joe <joemmalatesta@gmail.com>
2024-10-13 07:44:19 -0400

    remove toast shit

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import Book from "@/models/Book"; // Adjust the path as necessary
 import { mongoConnect } from "@/lib/mongodb";
-import { toast } from "react-toastify"; // Import the toast function
 
 export async function POST(request: Request) {
   await mongoConnect(); // Connect to the database
@@ -23,11 +22,9 @@ export async function POST(request: Request) {
   try {
     await book.save(); // Save the book to the database\
     console.log("Saved!!");
-    toast.success("Book added successfully!"); // Add success toast
     return NextResponse.json({ done: true }, { status: 200 });
   } catch (error) {
     console.error("Error saving book:", error);
-    toast.error("Failed to save book."); // Add error toast
     return NextResponse.json(
       { error: "Failed to save book." },
       { status: 500 }
@@ -53,11 +50,9 @@ export async function GET(request: NextRequest) {
     const booksWithImages = books.filter(
       (book) => book.image && book.image.trim() !== ""
     );
-    toast.success("Books fetched successfully!"); // Add success toast
     return NextResponse.json(booksWithImages, { status: 200 });
   } catch (error) {
     console.error("Error fetching books:", error);
-    toast.error("Failed to fetch books."); // Add error toast
     return NextResponse.json(
       { error: "Failed to fetch books." },
       { status: 500 }
@@ -71,18 +66,17 @@ export async function PUT(request: NextRequest) {
 
   try {
       await mongoConnect(); // Connect to the database
+
     const body = await request.json();
     const { checkedOutTo, isbn } = body;
 
     if (!checkedOutTo) {
-      toast.error("checkedOutTo is required."); // Add error toast
       return NextResponse.json(
         { error: "checkedOutTo is required in the request body." },
         { status: 400 }
       );
     }
     if (!isbn) {
-        toast.error("ISBN is required."); // Add error toast
         return NextResponse.json({ error: "ISBN is required." }, { status: 400 });
       }
 
@@ -93,15 +87,12 @@ export async function PUT(request: NextRequest) {
     );
 
     if (!updatedBook) {
-      toast.error("Book not found."); // Add error toast
       return NextResponse.json({ error: "Book not found." }, { status: 404 });
     }
 
-    toast.success("Book updated successfully!"); // Add success toast
     return NextResponse.json(updatedBook, { status: 200 });
   } catch (error) {
     console.error("Error updating book:", error);
-    toast.error("Failed to update book."); // Add error toast
     return NextResponse.json(
       { error: "Failed to update book." },
       { status: 500 }
