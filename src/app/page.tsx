97f73bb3978c635e45d2c3f148842f6fff1ea5d8
halee-t <haleetisler@gmail.com>
2024-10-13 06:38:02 -0400

    tiny touches to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,86 +13,101 @@ import Link from "next/link";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
-	const genres: string[] = ["children's", "Fantasy", "Fiction", "Historical Fiction", "Mystery", "Non-fiction", "Romance", "Sci-Fi", "Thriller"];
+  const genres: string[] = [
+    "Children's",
+    "Fantasy",
+    "Fiction",
+    "Historical Fiction",
+    "Mystery",
+    "Non-fiction",
+    "Romance",
+    "Sci-Fi",
+    "Thriller",
+  ];
 
-	const [isNewBookModalOpen, setIsNewBookModalOpen] = useState(false);
-	const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+  const [isNewBookModalOpen, setIsNewBookModalOpen] = useState(false);
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
 
-	const openNewBookModal = () => setIsNewBookModalOpen(true);
-	const closeNewBookModal = () => setIsNewBookModalOpen(false);
+  const openNewBookModal = () => setIsNewBookModalOpen(true);
+  const closeNewBookModal = () => setIsNewBookModalOpen(false);
 
-	return (
-		<main>
-			<section className="book-browsing-section">
-				<Navbar />
-				<section className="main-content w-full">
-					<div className="content-wrapper w-full">
-						<aside className="sidebar w-1/3">
-							<div className="filter-box">
-								<h3 className="filter-title">Location</h3>
-								<div className="filter-option">
-									<input type="checkbox" id="filter1"></input>
-									<label htmlFor="filter1">&lt; 5 mi</label>
-								</div>
-								<div className="filter-option">
-									<input type="checkbox" id="filter2"></input>
-									<label htmlFor="filter2">10 mi</label>
-								</div>
-								<div className="filter-option">
-									<input type="checkbox" id="filter3"></input>
-									<label htmlFor="filter3">25 mi</label>
-								</div>
-								<div className="filter-option">
-									<input type="checkbox" id="filter4"></input>
-									<label htmlFor="filter4">50 mi</label>
-								</div>
-							</div>
-						</aside>
-						<section className="main-section w-2/3">
-							<div className="search-container">
-								<div className="search-bar">
-									<img
-										src="https://cdn.builder.io/api/v1/image/assets/TEMP/3219361867156b40f47e4282855b0f72e161aeab4826f9fb6d1012a5d255f21c?placeholderIfAbsent=true&apiKey=61c5806b97e2479f978f0dd7f873e640"
-										alt="Search icon"
-										className="search-icon"
-									/>
-									<div className="flex">
-										<span className="search-text">Browse Books...</span>
-										<ul className="flex flex-wrap gap-1.5">
-											{genres.map((genre, index) => (
-												<li key={index} className="genre-item">
-													<input
-														type="checkbox"
-														id={`genre${index}`}
-														className="hidden"
-														onChange={() => {
-															if (selectedGenres.includes(genre)) {
-																setSelectedGenres(selectedGenres.filter((g) => g !== genre));
-															} else {
-																setSelectedGenres([...selectedGenres, genre]);
-															}
-														}}
-														checked={selectedGenres.includes(genre)}
-													/>
-													<label
-														htmlFor={`genre${index}`}
-														className={`inline-block px-4 py-2 rounded-full border border-gray-300 cursor-pointer transition-colors duration-200 ease-in-out hover:bg-gray-100 ${
-															selectedGenres.includes(genre) ? "bg-gray-200" : ""
-														}`}
-													>
-														{genre}
-													</label>
-												</li>
-											))}
-										</ul>
-									</div>
-								</div>
-							</div>
-							<BooksList />
-						</section>
-					</div>
-				</section>
-			</section>
-		</main>
-	);
+  return (
+    <main>
+      <section className="book-browsing-section">
+        <Navbar />
+        <section className="main-content w-full">
+          <div className="content-wrapper w-full">
+            <aside className="sidebar w-1/3">
+              <div className="filter-box">
+                <h3 className="filter-title">Location</h3>
+                <div className="filter-option">
+                  <input type="checkbox" id="filter1"></input>
+                  <label htmlFor="filter1">&lt; 5 mi</label>
+                </div>
+                <div className="filter-option">
+                  <input type="checkbox" id="filter2"></input>
+                  <label htmlFor="filter2">10 mi</label>
+                </div>
+                <div className="filter-option">
+                  <input type="checkbox" id="filter3"></input>
+                  <label htmlFor="filter3">25 mi</label>
+                </div>
+                <div className="filter-option">
+                  <input type="checkbox" id="filter4"></input>
+                  <label htmlFor="filter4">50 mi</label>
+                </div>
+              </div>
+              <Map />
+            </aside>
+            <section className="main-section w-2/3">
+              <div className="search-container">
+                <div className="search-bar">
+                  <img
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/3219361867156b40f47e4282855b0f72e161aeab4826f9fb6d1012a5d255f21c?placeholderIfAbsent=true&apiKey=61c5806b97e2479f978f0dd7f873e640"
+                    alt="Search icon"
+                    className="search-icon"
+                  />
+                  <div className="flex">
+                    <span className="search-text">Browse Books...</span>
+                    <ul className="flex flex-wrap gap-1.5 genre-selections">
+                      {genres.map((genre, index) => (
+                        <li key={index} className="genre-item">
+                          <input
+                            type="checkbox"
+                            id={`genre${index}`}
+                            className="hidden"
+                            onChange={() => {
+                              if (selectedGenres.includes(genre)) {
+                                setSelectedGenres(
+                                  selectedGenres.filter((g) => g !== genre)
+                                );
+                              } else {
+                                setSelectedGenres([...selectedGenres, genre]);
+                              }
+                            }}
+                            checked={selectedGenres.includes(genre)}
+                          />
+                          <label
+                            htmlFor={`genre${index}`}
+                            className={`inline-block px-4 py-2 rounded-full border border-gray-300 cursor-pointer transition-colors duration-200 ease-in-out hover:bg-gray-100 ${
+                              selectedGenres.includes(genre)
+                                ? "bg-gray-200"
+                                : ""
+                            }`}
+                          >
+                            {genre}
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <BooksList />
+            </section>
+          </div>
+        </section>
+      </section>
+    </main>
+  );
 }
