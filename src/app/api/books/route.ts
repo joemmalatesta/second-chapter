de8d7aaed75e8c9380fadd51bbfba382d689c501
Joe <joemmalatesta@gmail.com>
2024-10-12 23:26:10 -0400

    saving books works lessgooo

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -2,10 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import Book from "../../../models/Book"; // Adjust the path as necessary
 import { mongoConnect } from "@/lib/mongodb";
 
-export async function POST(req: NextRequest) {
+export async function POST(request: Request) {
   await mongoConnect(); // Connect to the database
 
-  const { isbn, location, bookState, checkedOutTo, ownerId } = await req.json(); // Parse the JSON body
+  const { isbn, location, bookState, checkedOutTo, ownerId } = await request.json(); // Parse the JSON body
+  console.log(isbn)
 
   const book = new Book({
     isbn,
@@ -17,14 +18,7 @@ export async function POST(req: NextRequest) {
 
   try {
     await book.save(); // Save the book to the database
-    console.log(
-      "Book saved:",
-      isbn,
-      location,
-      bookState,
-      checkedOutTo,
-      ownerId
-    );
+    console.log("Saved!!")
     return NextResponse.json({ done: true }, { status: 200 });
   } catch (error) {
     console.error("Error saving book:", error);
@@ -34,53 +28,3 @@ export async function POST(req: NextRequest) {
     );
   }
 }
-
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import Book from "../../../models/Book"; // Ensure the path is correct
-// import dbConnect from "../../../lib/dbConnect";
-
-// interface BookData {
-//   isbn: string;
-//   location: string;
-//   bookState: string;
-//   checkedOutTo: string;
-//   ownerId: string;
-// }
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   if (req.method === "POST") {
-//     console.log("Received a POST request to /api/books");
-
-//     await sendBooks(req, res);
-//   } else {
-//     res.setHeader("Allow", ["POST"]);
-//     res.status(405).end(`Method ${req.method} Not Allowed`);
-//   }
-// }
-
-// const sendBooks = async (req: NextApiRequest, res: NextApiResponse) => {
-//   await dbConnect();
-
-//   const { isbn, location, bookState, checkedOutTo, ownerId }: BookData =
-//     req.body;
-
-//   const book = new Book({
-//     isbn,
-//     location,
-//     bookState,
-//     checkedOutTo,
-//     ownerId,
-//   });
-
-//   try {
-//     await book.save();
-//     console.log("inside api", isbn, location, bookState, checkedOutTo, ownerId);
-//     res.status(200).json({ done: true });
-//   } catch (error) {
-//     console.error("Error saving book:", error);
-//     res.status(500).json({ error: "Failed to save book." });
-//   }
-// };
--- src/app/components/NewBookModal.tsx
+++ src/app/components/NewBookModal.tsx
@@ -5,162 +5,143 @@ import { useEffect, useState } from "react";
 
 //LOCATION
 interface GeolocationData {
-  latitude: number;
-  longitude: number;
-  accuracy: number;
+	latitude: number;
+	longitude: number;
+	accuracy: number;
 }
 
 //BOOK API
 interface BookDetails {
-  fetchedTitle: string;
-  author: string;
-  genre: string;
-  thumbnail: string | null;
-  identifier: string;
+	fetchedTitle: string;
+	author: string;
+	genre: string;
+	thumbnail: string | null;
+	identifier: string;
 }
 
-const NewBookModal = () => {
-  const [location, setLocation] = useState<GeolocationData | null>(null);
-  const [error, setError] = useState<string | null>(null);
+let NewBookModal = () => {
+	let [location, setLocation] = useState<GeolocationData | null>(null);
+	let [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (!navigator.geolocation) {
-      setError("Geolocation is not supported by your browser");
-      return;
-    }
+	useEffect(() => {
+		if (!navigator.geolocation) {
+			setError("Geolocation is not supported by your browser");
+			return;
+		}
 
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          accuracy: position.coords.accuracy,
-        });
-      },
-      (error) => {
-        setError(`Error: ${error.message}`);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-      }
-    );
-  }, []);
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				setLocation({
+					latitude: position.coords.latitude,
+					longitude: position.coords.longitude,
+					accuracy: position.coords.accuracy,
+				});
+			},
+			(error) => {
+				setError(`Error: ${error.message}`);
+			},
+			{
+				enableHighAccuracy: true,
+				timeout: 5000,
+				maximumAge: 0,
+			}
+		);
+	}, []);
 
-  const [bookTitle, setBookTitle] = useState("");
-  const [bookDetails, setBookDetails] = useState<BookDetails | null>(null);
+	let [bookTitle, setBookTitle] = useState("");
+	let [bookDetails, setBookDetails] = useState<BookDetails | null>(null);
 
-  const bookState = "new";
-  const checkedOutTo = "haleetisler@gmail.com";
-  const ownerId = "670b22bfe524b61f60083d44";
+	let bookState = "new";
+	let checkedOutTo = "haleetisler@gmail.com";
+	let ownerId = "670b22bfe524b61f60083d44";
 
-  // const title = "To Kill A Mockingbird";
+	// let title = "To Kill A Mockingbird";
 
-  const fetchBookDetails = async (title: string) => {
-    const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}`
-    );
-    const data = await response.json();
-    return data.items[0] ? data.items[0] : null;
-  };
+	let fetchBookDetails = async (title: string) => {
+		let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${title}`);
+		let data = await response.json();
+		return data.items[0] ? data.items[0] : null;
+	};
 
-  const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // Prevent the default form submission behavior
-    try {
-      const bookDetails = await fetchBookDetails(bookTitle); //fetch book details using title
+	let handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault(); // Prevent the default form submission behavior
+		try {
+			let fetchedBook = await fetchBookDetails(bookTitle); //fetch book details using title
+			if (!fetchedBook) {
+				console.error("Book not found");
+				return;
+			}
+			setBookDetails({
+				fetchedTitle: fetchedBook.volumeInfo.title,
+				identifier: fetchedBook['volumeInfo']['industryIdentifiers'][0]['identifier'],
+				author: fetchedBook.volumeInfo.authors ? fetchedBook.volumeInfo.authors.join(", ") : "Unknown",
+				genre: fetchedBook.volumeInfo.categories ? fetchedBook.volumeInfo.categories.join(", ") : "Unknown",
+				thumbnail: fetchedBook.volumeInfo.imageLinks ? fetchedBook.volumeInfo.imageLinks.thumbnail : null
+			});
+            console.log(bookDetails)
+		} catch (error) {
+			console.error("problem", error);
+		}
+	};
 
-      if (!bookDetails) {
-        console.error("Book not found");
-        return;
-      }
+	let sendemail = async () => {
+		let response = await fetch("/api/claim", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	};
 
-      const fetchedTitle = bookDetails.volumeInfo.title; // Extract title
-      const identifier = bookDetails.volumeInfo.identifier;
-      const author = bookDetails.volumeInfo.authors
-        ? bookDetails.volumeInfo.authors.join(", ")
-        : "Unknown"; // Extract author(s)
-      const genre = bookDetails.volumeInfo.categories
-        ? bookDetails.volumeInfo.categories.join(", ")
-        : "Unknown";
-      const thumbnail = bookDetails.volumeInfo.imageLinks
-        ? bookDetails.volumeInfo.imageLinks.thumbnail
-        : null; // Extract thumbnail
+	let sendbooksdb = async () => {
+		if (!bookDetails) {
+			console.log("No book details to save.");
+			return;
+		}
+		console.log(bookDetails.identifier);
+		let response = await fetch("/api/books", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				isbn: bookDetails.identifier,
+				location,
+				bookState,
+				checkedOutTo,
+				ownerId,
+			}),
+		});
+	};
 
-      setBookDetails({ fetchedTitle, author, genre, thumbnail, identifier });
-    } catch (error) {
-      console.error("problem", error);
-    }
-  };
+	return (
+		<main>
+			<form onSubmit={handleSearch}>
+				<label>Search for a Book:</label>
+				<br></br>
+				<input type="text" value={bookTitle} onChange={(e) => setBookTitle(e.target.value)}></input>
+				<button type="submit">FIND IT</button>
+			</form>
 
-  const sendemail = async () => {
-    const response = await fetch("/api/claim", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  };
+			{bookDetails && (
+				<div>
+					<h2>Book Title: {bookDetails.fetchedTitle}</h2>
+					<h3>Author: {bookDetails.author}</h3>
+					<h4>Genre: {bookDetails.genre}</h4>
+					{bookDetails.thumbnail && <Image src={bookDetails.thumbnail} alt={bookDetails.fetchedTitle} width={150} height={225} />}
+				</div>
+			)}
 
-  const sendbooksdb = async () => {
-    if (!bookDetails) {
-      console.log("No book details to save.");
-      return;
-    }
-    const response = await fetch("/api/books", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        isbn: bookDetails.identifier,
-        location,
-        bookState,
-        checkedOutTo,
-        ownerId,
-      }),
-    });
-  };
+			<br></br>
+			<br></br>
+			<button onClick={sendemail}>Email Me</button>
 
-  return (
-    <main>
-      <form onSubmit={handleSearch}>
-        <label>Search for a Book:</label>
-        <br></br>
-        <input
-          type="text"
-          value={bookTitle}
-          onChange={(e) => setBookTitle(e.target.value)}
-        ></input>
-        <button type="submit">FIND IT</button>
-      </form>
-
-      {bookDetails && (
-        <div>
-          <h2>Book Title: {bookDetails.fetchedTitle}</h2>
-          <h3>Author: {bookDetails.author}</h3>
-          <h4>Genre: {bookDetails.genre}</h4>
-          {bookDetails.thumbnail && (
-            <Image
-              src={bookDetails.thumbnail}
-              alt={bookDetails.fetchedTitle}
-              width={150}
-              height={225}
-            />
-          )}
-        </div>
-      )}
-
-      <br></br>
-      <br></br>
-      <button onClick={sendemail}>Email Me</button>
-
-      <br></br>
-      <br></br>
-      <br></br>
-      <button onClick={sendbooksdb}>Book Database</button>
-    </main>
-  );
+			<br></br>
+			<br></br>
+			<br></br>
+			<button onClick={sendbooksdb}>Book Database</button>
+		</main>
+	);
 };
 
 export default NewBookModal;
